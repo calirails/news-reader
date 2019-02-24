57caa4354427329fcd59b3c94ce7137215fbc269
calvin <calvin98115@yahoo.com>
2019-02-24 08:49:20 -0800

    changed to dark-mode skin and reformatted spaces into tabs

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,90 @@
 <template>
-  <v-app light>
-    <!--The SideBar menu component go here -->
-    <SideBar :NewsAPIKey='NewsAPIKey' :drawer='drawer' 
-      v-on:selectSource='setNewsChannel'/>
-    <v-toolbar fixed app light clipped-left color="primary" class="elevation-2">
-      <v-toolbar-side-icon @click="drawer = !drawer" class="white--text"></v-toolbar-side-icon>
-      <v-toolbar-title class="white--text">News App</v-toolbar-title>
-    </v-toolbar>
-    <v-content>
-      <v-container fluid>
-        <!--The Main Content component go here-->
-        <NewsArticles :articles='articles' />
-      </v-container>
-    </v-content>
-    <v-footer class="secondary" app>
-      <v-layout row wrap align-center>
-        <v-flex xs12>
-          <div class="white--text ml-3">
-            Made with
-            <v-icon class="red--text">favorite</v-icon>by
-            <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
-            and
-            <a
-              class="white--text"
-              href="https://github.com/rachidsakara"
-              target="_blank"
-            >Rachid Sakara</a>
-          </div>
-        </v-flex>
-      </v-layout>
-    </v-footer>
-  </v-app>
+    <v-app dark>
+        <!--Top Menu Navigation -->
+        <v-toolbar fixed app light clipped-left color="black" class="elevation-2">
+            <v-toolbar-side-icon @click="drawer=!drawer" class="white--text"></v-toolbar-side-icon>
+            <v-toolbar-title class="white--text">News App</v-toolbar-title>
+        </v-toolbar>
+        <!--The SideBar menu component go here -->
+        <SideBar v-bind:NewsAPIKey='NewsAPIKey' 
+            v-bind:drawer='drawer' 
+            v-on:selectSource='setNewsChannel'/>
+        <!--Main Content -->
+        <v-content>
+            <v-container fluid>
+                <!--The Main Content component go here-->
+                <NewsArticles :articles='articles' />
+            </v-container>
+        </v-content>
+        <v-footer class="secondary" app>
+            <v-layout row wrap align-center>
+                <v-flex xs12>
+                    <div class="white--text ml-3">
+                        Made with
+                        <v-icon class="red--text">favorite</v-icon>by
+                        <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
+                        and
+                        <a
+                          class="white--text"
+                          href="https://github.com/rachidsakara"
+                          target="_blank"
+                        >Rachid Sakara</a>
+                    </div>
+                </v-flex>
+            </v-layout>
+        </v-footer>
+    </v-app>
 </template>
 <script>
-  import Axios from 'axios'
-  import SideBar from '@/components/SideBar'
-  import NewsArticles from '@/components/NewsArticles.vue'
-  // const NewsAPIKey = 'b8f2d3ccc2de4b108a18205b2ae9d2c6'
+    import Axios from 'axios'
+    import SideBar from '@/components/SideBar'
+    import NewsArticles from '@/components/NewsArticles.vue'
+    // const NewsAPIKey = 'b8f2d3ccc2de4b108a18205b2ae9d2c6'
   
-  export default {
-    components: {
-      NewsArticles,
-      SideBar
-    },
-    data() {
-      return {
-        NewsAPIKey:  'b8f2d3ccc2de4b108a18205b2ae9d2c6', 
-        drawer: true,
-        articles: [],
-        errors: []
-      };
-    },
-    async created() {
-      const requestUrl = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.NewsAPIKey}`
-      console.log(`RequestUrl: ${requestUrl}`)
-      await Axios.get(requestUrl)
-        .then(response => {
-          console.log(`Response of articles: ${response.data.articles}`)
-          this.articles = response.data.articles
-        }).catch(error => this.errors.push(error))
-    },
-    methods: {
-      setNewsChannel(channelId) {
-        const requestUrl = `https://newsapi.org/v2/top-headlines?sources=${channelId}&apiKey=${this.NewsAPIKey}`
-        console.log(`RequestUrl: ${requestUrl}`)
-        Axios.get(requestUrl)
-          .then(response => this.articles = response.data.articles)
-          .catch(error => {
-            console.log(`Fetch News for channel error: ${error}`)
-            this.errors.push(error)
-          })
-      }
-    }
-  };
+    export default {
+        components: {
+            NewsArticles,
+            SideBar
+          },
+        data() {
+            return {
+                NewsAPIKey:  'b8f2d3ccc2de4b108a18205b2ae9d2c6', 
+                drawer: true,
+                articles: [],
+                errors: []
+            };
+        },
+        async created() {
+            const requestUrl = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.NewsAPIKey}`
+            console.log(`RequestUrl: ${requestUrl}`)
+            await Axios.get(requestUrl)
+                .then(response => {
+                    console.log(`Response of articles: ${response.data.articles}`)
+                    this.articles = response.data.articles
+                }).catch(error => this.errors.push(error))
+        },
+        methods: {
+            setNewsChannel(channelId) {
+                const requestUrl = `https://newsapi.org/v2/top-headlines?sources=${channelId}&apiKey=${this.NewsAPIKey}`
+                console.log(`RequestUrl: ${requestUrl}`)
+                Axios.get(requestUrl)
+                  .then(response => this.articles = response.data.articles)
+                  .catch(error => {
+                    console.log(`Fetch News for channel error: ${error}`)
+                    this.errors.push(error)
+                  })
+            }
+        }
+    };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,19 +1,23 @@
 <template>
-  <v-navigation-drawer v-model="drawer" fixed app clipped class="drawer-style" id="style-1">
-    <v-list dense class="pt-3 white--text">
-      <v-list-tile v-for="source in sources" :key="source.id" @click="selectSource(source.id)">
-        <v-list-tile-action>
-          <v-avatar size="32px">
-            <img class="img-circle elevation-7 mb-1" :src="getImgUrl(source.id)">
-          </v-avatar>
-        </v-list-tile-action>
-
-        <v-list-tile-content>
-          <v-list-tile-title>{{ source.name }}</v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </v-list>
-  </v-navigation-drawer>
+    <v-navigation-drawer v-model="drawer" 
+        fixed 
+        app 
+        clipped 
+        class="drawer-style" 
+        id="style-1">
+        <v-list dense class="pt-3 white--text">
+            <v-list-tile v-for="source in sources" :key="source.id" @click="selectSource(source.id)">
+                <v-list-tile-action>
+                    <v-avatar size="32px">
+                        <img class="img-circle elevation-7 mb-1" :src="getImgUrl(source.id)">
+                    </v-avatar>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>{{ source.name }}</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+    </v-navigation-drawer>
 </template>
 
 
@@ -74,8 +78,8 @@ export default {
     -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
     background-color: #F5F5F5;
   }
-  .drawer-style {
+  /* .drawer-style {
     background-color: #2196F3 !important;
     border-color: #2196F3 !important;
-  }
+  } */
 </style>
